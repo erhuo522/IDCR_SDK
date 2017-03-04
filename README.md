e311873216d71427e32523b47266d33a51a095ec
machfe <mni2005@163.com>
2017-03-04 18:38:34 +0800

    initialize project

---
+++ README.md
@@ -0,0 +1,4 @@
+ID Card Recognition（IDCR）
+
+author: machfe
+email: mni2005@163.com
---
+++ src/Demo.cpp
@@ -0,0 +1,11 @@
+// IDCR_Demo.cpp : 定义控制台应用程序的入口点。
+//
+
+#include  <stdio.h>
+
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
+
